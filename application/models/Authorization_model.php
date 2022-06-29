53284d6ca40bbcbf31281fa84f3db21e6d087935
Ajay Yadav <ajay.yadav@woco.co.in>
2022-06-29 11:09:00 +0530

    to created and check autorisation

---
+++ application/models/Authorization_model.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Authorization_model extends CI_Model {
+
+
+public function login_user($email, $password)
+{
+
+	echo "<pre>";
+	print_r($email);
+	die();
+
+
+}
+
+}
