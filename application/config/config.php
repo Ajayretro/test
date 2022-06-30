ea86bf7032d30317e82a90cf0a4594a9fe3f47d6
Ajay Yadav <ajay.yadav@woco.co.in>
2022-06-30 09:55:53 +0530

    again push

--- application/config/config.php
+++ application/config/config.php
@@ -328,7 +328,8 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/userguide3/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+
+$config['encryption_key'] = 'VkYp3s6v9y$B?E(H';
 
 /*
 |--------------------------------------------------------------------------
