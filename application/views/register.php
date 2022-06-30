4efff68546a2b8cee4f9edb319a7f8f288b7a126
Ajay Yadav <ajay.yadav@woco.co.in>
2022-06-30 09:46:44 +0530

    now again push the same after revert

--- application/views/register.php
+++ application/views/register.php
@@ -49,7 +49,7 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Codeiniter Testing Project !</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
 
                                     <form class="user" method="post" action="authorization/register" id="id_form_submit" >
@@ -63,21 +63,22 @@
                                                 id="id_register_password" name="register_password" placeholder="Password">
                                         </div>
                                         <div class="form-group">
-                                            <label class="container">Remember Me
-                                              <input type="checkbox" checked="checked" value="1" name="checkbox_remember" id="id_checkbox_remember">
-                                              <span class="checkmark"></span>
-                                            </label>
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="id_remeber_checkbox" name="register_checkbox">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
                                         </div>
                                         <button  class="btn btn-primary btn-user btn-block" type="submit" >
                                             Login
                                         </button>
-                                        <!-- <hr> -->
-                                        <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
+                                        <hr>
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
                                         <a href="index.html" class="btn btn-facebook btn-user btn-block">
                                             <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a> -->
+                                        </a>
                                     </form>
 
                                     <hr>
